03940349e3dfeb1b6eea98dff3fbd0b7d946b7ce
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-13 17:48:52 +0900

    add host

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -1,13 +1,20 @@
-use alloc::string::{String, ToString};
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Url {
     url: String,
+    host: String,
 }
 
 impl Url {
     pub fn new(url: String) -> Self {
-        Self { url }
+        Self {
+            url,
+            host: "".to_string(),
+        }
     }
 
     fn is_http(&self) -> bool {
@@ -17,12 +24,31 @@ impl Url {
         false
     }
 
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if let Some(index) = url_parts[0].find(":") {
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+
     pub fn parse(&mut self) -> Result<Self, String> {
         if !self.is_http() {
             return Err("Only HTTP scheme is supported".to_string());
         }
 
+        self.host = self.extract_host();
         Ok(self.clone())
+    }
+
+    pub fn host(&self) -> String {
+        self.host.clone()
     }
 }
 
@@ -50,8 +76,27 @@ mod tests {
             let url = "http://example.com".to_string();
             let expected = Ok(Url {
                 url: "http://example.com".to_string(),
+                host: "example.com".to_string(),
             });
             assert_eq!(Url::new(url).parse(), expected)
         }
+    }
+
+    #[test]
+    fn extract_host_from_url() {
+        assert_eq!(
+            Url::new("http://example.com".to_string())
+                .parse()
+                .unwrap()
+                .host(),
+            "example.com".to_string()
+        );
+        assert_eq!(
+            Url::new("http://github.com:8080/foo/bar?page=2&order=asc".to_string())
+                .parse()
+                .unwrap()
+                .host(),
+            "github.com".to_string()
+        );
     }
 }
