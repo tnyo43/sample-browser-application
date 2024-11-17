6f6b9b91a00f0b0e08e7895bb7d5754cab3e5474
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-17 18:47:31 +0900

    add after attribute name pattern

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -252,6 +252,34 @@ impl Iterator for HtmlTokenizer {
 
                     self.append_attribute(c, true);
                 }
+                State::AfterAttributeName => {
+                    if c == ' ' {
+                        continue;
+                    }
+
+                    if c == '/' {
+                        self.state = State::SelfClosingStartTag;
+                        continue;
+                    }
+
+                    if c == '=' {
+                        self.state = State::BeforeAttributeValue;
+                        continue;
+                    }
+
+                    if c == '>' {
+                        self.state = State::Data;
+                        return self.take_latest_token();
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    self.re_consume = true;
+                    self.state = State::AttributeName;
+                    self.start_new_attribute();
+                }
                 _ => {
                     return None;
                 }
