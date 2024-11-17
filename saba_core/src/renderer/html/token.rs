6dc4be5d237e37ec6f91b1097e6dfdd2e55d4d72
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-17 16:58:35 +0900

    add Data pattern

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -37,9 +37,21 @@ enum State {
 }
 
 pub struct HtmlTokenizer {
-    input: String,
+    input: Vec<char>,
     pos: usize,
     state: State,
+}
+
+impl HtmlTokenizer {
+    fn consume_next_input(&mut self) -> char {
+        let c = self.input[self.pos];
+        self.pos += 1;
+        c
+    }
+
+    fn is_eof(&self) -> bool {
+        self.pos == self.input.len()
+    }
 }
 
 impl Iterator for HtmlTokenizer {
@@ -51,9 +63,20 @@ impl Iterator for HtmlTokenizer {
         };
 
         loop {
+            let c = self.consume_next_input();
+
             match self.state {
                 State::Data => {
-                    return None;
+                    if c == '<' {
+                        self.state = State::TagOpen;
+                        continue;
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    return Some(HtmlToken::Char(c));
                 }
                 _ => {
                     return None;
