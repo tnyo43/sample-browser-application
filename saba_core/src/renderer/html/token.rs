13ff9cb7fc3e8e86c4a01eaa9c92623b2d6d0ff6
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-17 16:38:07 +0900

    add token

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -1,1 +1,18 @@
+use alloc::{string::String, vec::Vec};
+
+use super::attribute::Attribute;
+
+pub enum HtmlToken {
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    EndTag {
+        tag: String,
+    },
+    Char(char),
+    Eof,
+}
+
 pub struct HtmlTokenizer {}
