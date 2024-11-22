dbe528d23f719fa19731db95f5c6e4cca6b2debb
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-22 21:56:02 +0900

    add WasabiUI::new

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,7 +1,28 @@
-use noli::window::Window;
-use saba_core::browser::Browser;
+use alloc::{rc::Rc, string::ToString};
+use core::{cell::RefCell, fmt::Error};
+use noli::{error::Result as OsResult, window::Window};
+use saba_core::{browser::Browser, error::Error};
+
+use crate::constants::{WHITE, WINDOW_HEIGHT, WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS, WINDOW_WIDTH};
 
 pub struct WasabiUI {
     browser: Rc<RefCell<Browser>>,
     window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
 }
---
+++ ui/wasabi/src/constants.rs
@@ -0,0 +1,7 @@
+pub static WHITE: u32 = 0xffffff;
+
+pub static WINDOW_INIT_X_POS: i64 = 30;
+pub static WINDOW_INIT_Y_POS: i64 = 50;
+pub static WINDOW_WIDTH: i64 = 600;
+pub static WINDOW_HEIGHT: i64 = 400;
+pub static WINDOW_PADDING: i64 = 5;
