d1091986f2baefa9085f20728fb8ae8ebf7364e5
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-27 19:11:18 +0900

    add iterator

--- saba_core/src/renderer/css/token.rs
+++ saba_core/src/renderer/css/token.rs
@@ -1,6 +1,6 @@
-use alloc::string::String;
+use alloc::{string::String, vec::Vec};
 
-pub enum Token {
+pub enum CssToken {
     HashToken(String),
     Delim(char),
     Number(f64),
@@ -13,4 +13,37 @@ pub enum Token {
     Ident(String),
     StringToken(String),
     AtKeyword(String),
+}
+
+pub struct CssTokenizer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl CssTokenizer {
+    pub fn new(css: String) -> Self {
+        Self {
+            pos: 0,
+            input: css.chars().collect(),
+        }
+    }
+}
+
+impl Iterator for CssTokenizer {
+    type Item = CssToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.pos >= self.input.len() {
+                return None;
+            }
+
+            let token = match self.input[self.pos] {
+                _ => todo!(),
+            };
+            self.pos += 1;
+
+            return Some(token);
+        }
+    }
 }
