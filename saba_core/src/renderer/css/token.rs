9b5c64cfcab5b3cae266a22e4677fc5a338d3091
Tomoya Kashifuku <tnyo43@gmail.com>
2024-11-27 19:00:15 +0900

    add Tokens

---
+++ saba_core/src/renderer/css/token.rs
@@ -0,0 +1,16 @@
+use alloc::string::String;
+
+pub enum Token {
+    HashToken(String),
+    Delim(char),
+    Number(f64),
+    Colon,
+    SemiColon,
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenCurly,
+    CloseCurly,
+    Ident(String),
+    StringToken(String),
+    AtKeyword(String),
+}
